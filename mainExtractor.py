907707e55041115d87b2f46eb7abb7402b30910f
tldrd0117 <tldrd0117@gmail.com>
2019-08-04 21:52:16 +0900

    okok

--- mainExtractor.py
+++ mainExtractor.py
@@ -25,15 +25,44 @@ def printG(*msg):
 #StockLoader 시간에따른 주식 가격을 가져온다
 
 sl = StockLoader.create()
-topdf, topcap, sCode = sl.loadTopLately('2019-01-01','2019-08-04')
+topdf, topcap, sCode = sl.loadTopLately('2019-05-01','2019-08-04')
 # marcapdf = sl.loadMarcap()
 factordf = sl.loadFactor()
 ss = StockStrategy.create()
+st = StockTransaction.create(topdf)
+
+konexCode = ['076340','223220','186230','112190','214610','224880','086220','284610','272420','178600','114920','199150','183410','176750','220110','086080','200350','271850','302920','270020','163430','216400','126340','232680','285770','215050','110660','135270','162120','210610','302550','185190','217320','233250','103660','189330','189540','267060','279600','149010','281310','208890','158300','086460','266170','203400','064850','260870','236030','271400','179720','216280','140660','267810','270660','121060','084440','221800','210120','101360','270210','299670','219750','140290','217910','189350','136660','116100','262760','199800','217880','202960','276240','225860','065370','107640','222670','121060','224760','225220','221670','299480','220250','207230','224810','228180','229000','229500','092590','217950','211050','230400','180060','225850','044990','232680','226610','258050','234070','277880','266870','278990','135160','215570','233990','176560','232530','206950','067370','236340','237720','222160','284420','238500','240340','241510','058970','242420','066830','228760','242850','112190','244880','245030','229480','208850','245450','246250','247300','224020','212310','191600','250300','239890','251960','252370','251280','243870','253840','199290','148780','167380','258540','232830','258250','227420','260970','205290','242350','120780']
+blackList = list(map(lambda x : sCode[x] if x in sCode.keys() else '', konexCode))
+
+
+# In[2]:
+# In[232]:
+def isNumber(val):
+    return isinstance(val, (int, float, complex))
+def isnum(s):
+    try:
+        float(s)
+    except:
+        return(False)
+    else:
+        return(True)
+def toNumeric(df):
+    return df.applymap(lambda v : float(v) if isNumber(v) or isnum(v) else np.nan )
+for key in factordf.keys():
+    factordf[key].columns = list(map(lambda col : float(col) if isNumber(col) or col.isnumeric() else col, factordf[key].columns))
+for key in factordf.keys():
+    factordf[key] = factordf[key].set_index(['종목명'])
+for key in factordf.keys():
+    factordf[key] = toNumeric(factordf[key])
+# In[3]:
+
 
 current = pd.to_datetime('2019-08-04', format='%Y-%m-%d')
 
 
 target = list(topdf.columns)
+if len(blackList) > 0:
+    target = list(filter(lambda x : x not in blackList, target ))
 target = ss.filterAltmanZScore(current, topdf[target], factordf, topcap )
 printG('altman', len(target))
 # target = ss.getFactorLists(current, topdf[target], factordf, factors, 20, weights)
@@ -59,6 +88,8 @@ printG('momentumSum', momentumSum)
 if ss.isUnemployedYear(current.year):
     printG('isUnemployedYear')
     target = beforeTarget
+# In[4]:SHOW
 printG(target)
-        
-
+for code in target:
+    rate = st.calculateLosscutRate(code, current)
+    printG(code, rate)
