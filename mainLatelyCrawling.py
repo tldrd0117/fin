b20e0392af80e6d63ecce0d49bddfa3a7d90e26e
tldrd0117 <tldrd0117@gmail.com>
2019-12-02 21:28:55 +0900

    am

--- mainLatelyCrawling.py
+++ mainLatelyCrawling.py
@@ -12,8 +12,8 @@ sl = StockLoader.create()
 alreadyIn = []#stockDb.distinct('종목코드')
 print(alreadyIn)
 
-startDate = '2019-11-29'
-endDate = '2019-11-29'
+startDate = '2019-11-30'
+endDate = '2019-12-02'
 
 topcap, allCodes, allNames = sl.loadTopcapDf(maxMarketCap=float('inf'))
 
